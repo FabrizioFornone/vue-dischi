d2cb4b024dbfee8324e3c7cb4af736abc4f646c0
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-31 15:24:56 +0100

    Loader.vue component + loader.scss created

--- src/components/HeaderBox.vue
+++ src/components/HeaderBox.vue
@@ -1,15 +1,19 @@
 <template>
   <header>
-    <img class="mx-3 py-2" src="@/assets/logo-small.svg" alt="">
+    <div class="mx-3 py-2 d-flex justify-content-between">
+      <img class="" src="@/assets/logo-small.svg" alt="" />
+      <loader />
+    </div>
   </header>
 </template>
 
 <script>
+import Loader from "./Loader.vue";
 export default {
-
+  components: { Loader },
 };
 </script>
 
-<style lang="scss" scoped>
+<style lang="scss" scoped LoaderComponent>
 @import "@/style/headerBox.scss";
 </style>
---
+++ src/components/Loader.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>
+    <div class="lds-ring"><div></div><div></div><div></div><div></div></div>
+  </div>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/loader.scss";
+</style>
