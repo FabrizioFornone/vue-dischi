76f6c3adb5fc7b873df6614fdc474f3ee7fd878e
FabrizioFornone <fabrizio.fornone@hotmail.it>
2022-01-31 12:43:14 +0100

    Repo ok

---
+++ src/components/HeaderBox.vue
@@ -0,0 +1,13 @@
+<template>
+  <header>
+    <h1 class="text-center">test</h1>
+  </header>
+</template>
+
+<script>
+export default {};
+</script>
+
+<style lang="scss" scoped>
+@import "@/style/headerBox.scss";
+</style>
